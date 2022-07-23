51ab6c147bf456f4a4cc02503ad56e8d18a5abfa
Gifty <giftyikechukwu@gmail.com>
2022-07-23 09:54:46 +0100

    Delete create_static_lib.sh

--- 0x09-static_libraries/create_static_lib.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-<<<<<<< HEAD
-gcc -Wall -pedantic -Werror -Wextra -c *.c
-=======
-gcc -Wall -Werror -Wextra -pedantic -std=gnu89 *.c
->>>>>>> 98280615902e34a06b7d4d0e881f6e3c7ae5be76
-ar -rc liball.a *.o
-ranlib liball.a
